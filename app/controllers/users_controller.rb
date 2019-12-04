07899986f223a451eb4a68300ac8b97faa776057
kayochamu <pinkpc45@gmail.com>
2019-12-04 12:49:03 +0000

    １０.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,13 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
 
   def index
     @users = User.paginate(page: params[:page])
   end
 
   def show
-    @first_day = Date.current.beginning_of_month
-    @last_day = @first_day.end_of_month
   end
 
   def new
@@ -48,22 +47,24 @@ class UsersController < ApplicationController
 
   def edit_basic_info
   end
-  
-  
-def update_basic_info
-  if @user.update_attributes(basic_info_params)
-    flash[:success] = "#{@user.name}の基本情報を更新しました。"
-  else
-    flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+
+  def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+    redirect_to users_url
   end
-  redirect_to users_url
-end
-end
 
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+    end
+
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
     end
 
     # beforeフィルター
@@ -91,9 +92,4 @@ end
     def admin_user
       redirect_to root_url unless current_user.admin?
     end
-
-    def basic_info_params
-      params.require(:user).permit(:department, :basic_time, :work_time)
-    end
-    
-   
+end
